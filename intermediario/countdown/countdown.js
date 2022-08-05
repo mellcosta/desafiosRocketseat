131e18b49aa2bc8d144b6b4ee9c67b858a030a3a
mellcosta <m3licosta@gmail.com>
2022-08-05 23:44:26 +0100

    :rainbow: Countdown: Modal

---
+++ intermediario/countdown/countdown.js
@@ -0,0 +1,25 @@
+var countDate = new Date ('Jan 1, 2023 00:00:00').getTime();
+
+function newYear(){
+    var now = new Date().getTime();
+    gap = countDate - now;
+
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+    
+    var d = Math.floor(gap / (day));
+    var h = Math.floor((gap % (day)) / (hour));
+    var m = Math.floor((gap % (hour)) / (minute));
+    var s = Math.floor((gap % (minute)) / second);
+
+    document.getElementById('days').innerText = d;
+    document.getElementById('hours').innerText = h;
+    document.getElementById('minutes').innerText = m;
+    document.getElementById('seconds').innerText = s;
+
+    setInterval(function(){
+        newYear();
+    },1000)
+}
